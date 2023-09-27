0b5df6c8db88c8664922c179fef9670643724779
Mahsak89 <>
2023-09-27 06:34:28 +0000

    added signuoin and buttons style modules

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,21 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import styles from "../../styles/SignInUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
 import signUp from "../../assets/signup.jpg";
 
 import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
 
 const SignUpForm = () => {
   return (
-    <Row>
+    <Row className={styles.Row}>
       <Col className="my-auto py-2 p-md-2" md={6}>
-        <Container className="p-4">
-          <h1>sign up</h1>
+        <Container className={`${appStyles.Content} p-4 `}>
+          <h1 className={styles.Header}>sign up</h1>
 
           <Form>
             <Form.Group controlId="username">
               <Form.Label className="d-none">username</Form.Label>
               <Form.Control
+                className={styles.Input}
                 type="text"
                 placeholder="Username"
                 name="username"
@@ -25,6 +29,7 @@ const SignUpForm = () => {
             <Form.Group controlId="password1">
               <Form.Label className="d-none">Password</Form.Label>
               <Form.Control
+                className={styles.Input}
                 type="password"
                 placeholder="Password"
                 name="password1"
@@ -34,23 +39,32 @@ const SignUpForm = () => {
             <Form.Group controlId="password2">
               <Form.Label className="d-none">Confirm password</Form.Label>
               <Form.Control
+                className={styles.Input}
                 type="password"
                 placeholder="Confirm password"
                 name="password2"
               />
             </Form.Group>
 
-            <Button type="submit">Sign up</Button>
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+              type="submit"
+            >
+              Sign up
+            </Button>
           </Form>
         </Container>
-        <Container className="mt-3">
-          <Link to="/signin">
+        <Container className={`mt-3 ${appStyles.Content}`}>
+          <Link className={styles.Link} to="/signin">
             Already have an account? <span>Sign in</span>
           </Link>
         </Container>
       </Col>
-      <Col md={6} className="my-auto d-none d-md-block p-2">
-        <Image src={signUp} />
+      <Col
+        md={6}
+        className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
+      >
+        <Image className={`${appStyles.FillerImage}`} src={signUp} />
       </Col>
     </Row>
   );
