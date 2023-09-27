c31de64e523ad1047282c4adb34445498d30a960
Mahsak89 <>
2023-09-27 02:06:11 +0000

    added app style and initial NavaBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.module.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+      <NavBar />
     </div>
   );
 }
