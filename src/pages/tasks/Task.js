4e80935dd023ee552e971ffc8a51d9d0274c25e7
Mahsak89 <>
2023-09-30 19:44:42 +0000

    handle like and unlike

--- src/pages/tasks/Task.js
+++ src/pages/tasks/Task.js
@@ -3,6 +3,8 @@ import styles from "../../styles/Task.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Card} from "react-bootstrap";
 import { Link} from "react-router-dom";
+import { axiosRes } from "../../api/axiosDefaults";
+
 
 
 
@@ -23,12 +25,43 @@ const Task = (props) => {
        // states_count,
         TaskPage,
         TasksPage,
+        setTasks,
       } = props
     
       const currentUser = useCurrentUser();
       const is_owner = currentUser?.username === owner;
 
-     
+      const handleLike = async () => {
+        try {
+          const { data } = await axiosRes.post("/states/", { task: id });
+          setTasks((prevTasks) => ({
+            ...prevTasks,
+            results: prevTasks.results.map((task) => {
+              return task.id === id
+                ? { ...task, state_id: data.id }
+                : task;
+            }),
+          }));
+        } catch (err) {
+          //console.log(err);
+        }
+      };
+    
+      const handleUnlike = async () => {
+        try {
+          await axiosRes.delete(`/states/${state_id}/`);
+          setTasks((prevTasks) => ({
+            ...prevTasks,
+            results: prevTasks.results.map((task) => {
+              return task.id === id
+                ? { ...task, state_id: null }
+                : task;
+            }),
+          }));
+        } catch (err) {
+         // console.log(err);
+        }
+      };
     
 
       
@@ -38,18 +71,18 @@ const Task = (props) => {
  
 
   return (
-    <Card className={styles.Post}>
+    <Card className={styles.Task}>
         
         <Card.Body>          
           <div className="d-flex align-items-center ">
             <span>
                 { state_id ? (
-                <span >
-                    <i className={`fas fa-heart ${styles.Heart}`} />
+                <span  onClick={handleUnlike} >
+                    <i className={`fas fa-square ${styles.square}`} />
                 </span>
                 ) :  (
-                <span>
-                    <i className={`far fa-heart ${styles.HeartOutline}`} />
+                <span   onClick={handleLike}>
+                    <i className={`far fa-square ${styles.squareOutline}`} />
                 </span>
                 ) }
             </span>
