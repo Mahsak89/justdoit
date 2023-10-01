fda61805e20f8396343d791db8b54cd787294c0b
Mahsak89 <>
2023-10-01 07:40:53 +0000

    fixed some error

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -2,6 +2,7 @@ import { createContext, useContext, useMemo, useEffect, useState } from "react";
 import axios from "axios";
 import { axiosReq, axiosRes } from "../api/axiosDefaults";
 import { useHistory } from "react-router";
+import { removeTokenTimestamp, shouldRefreshToken } from "../utils/utils";
 
 export const CurrentUserContext = createContext();
 export const SetCurrentUserContext = createContext();
