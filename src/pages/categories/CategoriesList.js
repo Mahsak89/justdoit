310bb39ff3c12277f4322b0a3b59048c79e49d89
Mahsak89 <>
2024-02-03 17:50:59 +0000

    added edit and delet handle to categorieslist

--- src/pages/categories/CategoriesList.js
+++ src/pages/categories/CategoriesList.js
@@ -1,31 +1,42 @@
 import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import ListGroupItem from "react-bootstrap/ListGroupItem";
-import { Link } from 'react-router-dom'
-
-
-
+import { Link, useHistory } from 'react-router-dom'
 import { useRedirect } from "../../hooks/useRedirect";
-
-
-
 import appStyles from "../../App.module.css";
 import { useLocation } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
-
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { MoreDropdown } from "../../components/MoreDropdown";
 
 
-function CategorieList({ message, filter = "" }) {
-  useRedirect("loggedOut");
+function Cat("loggedOut");
 
   const currentUser = useCurrentUser();
-
   const [categories, setCategories] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
+  const history = useHistory();
+
+  
+  const handleEdit = (id) => {
+    history.push(`/categories/${id}/edit`);
+  };
+
+  const handleDelete = async (id) => {
+    try {
+        await axiosRes.delete(`/categories/${id}/`);
+        history.goBack();
+        setCategories((prevCategories) => ({
+          ...prevCategories,
+           results: prevCategories.results.filter((category) => category.id !== categoryId),
+        }));
+         catch (err) {
+        //console.log(err);
+    }
+  };
 
   useEffect(() => {
     const fetchCategories = async () => {
@@ -54,9 +65,13 @@ function CategorieList({ message, filter = "" }) {
                 children={ categories.results.map((category) => (
                     <ListGroupItem key={category.id}>
                         <Link to={`/categories/${category.id}`}>{category.name}</Link>
+                        <MoreDropdown
+                            handleEdit={() => handleEditCategory(category.id)}
+                            handleDelete={() => handleDeleteCategory(category.id)}
+                        />
                     </ListGroupItem>
                 ))}
-                dataLength={categories.results.length}
+                dataLength={categories.results.length} 
                 loader='...'
                 hasMore={!!categories.next}
                 next={() => fetchMoreData(categories, setCategories)}
